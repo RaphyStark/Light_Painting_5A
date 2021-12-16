8864819eccc28efbb08bb4ebbaccb73aa71db26b
Raphael KOSKAS <rkoskas@et.esiea.fr>
2021-12-16 14:49:05 +0100

    indentation OK

--- trajectoire/test.py
+++ trajectoire/test.py
@@ -31,36 +31,37 @@ robotTheta = 0
 OK = True
 while OK is True :
     success, frame = cap.read()
-    
-  # currentframe = 0
-	if not success :
+
+    # currentframe = 0
+    if not success :
         print("cap read not successed")
         OK = False
-        exit()
-    
-	else :         	
-		frame = cv.resize(frame, (round(cv.CAP_PROP_FRAME_WIDTH/100), round(cv.CAP_PROP_FRAME_HEIGHT/100)))   
+        #exit()
+
+    #(capX, capY) = (round(cv.CAP_PROP_FRAME_WIDTH/100), round(cv.CAP_PROP_FRAME_HEIGHT/100))
+    #dim = (capX, capY)
+
+    else :         	
+        frame = cv.resize(frame, (100, 100))
+        print("frame X = " + str(frame.shape[1]))
+        print("frame Y = " + str(frame.shape[0]))
 
 
-    print("frame X = " + str(frame.shape[1]))
-    print("frame Y = " + str(frame.shape[0]))
-    
-   
 
 
     gray_image = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     gray_image = cv.blur(gray_image, (5,3))
     success,thresh = cv.threshold(gray_image,127,255,0)
-    
+
     if not success :
-    	print("threshold not successed")
-    	exit()
-	
+        print("threshold not successed")
+        exit()
+
     cv.imshow('frame', frame)
     if cv.waitKey(1) == ord('q') :
         OK = False
-        
-    '''
+
+'''
     # find contours in the binary image
     contours, hierarchy = cv.findContours(thresh,cv.RETR_TREE,cv.CHAIN_APPROX_TC89_KCOS)
     for c in contours:
@@ -68,17 +69,17 @@ while OK is True :
         M = cv.moments(c)
         # calculate x,y coordinate of center           
         if (M["m00"] != 0) :
-            robotX_precedent = robotX
-            robotY_precedent = robotY
-            robotX = int(M["m10"] / M["m00"])    
-            robotY = int(M["m01"] / M["m00"])
-            robotOmega_precedent = robotOmega_precedent + robotOmega
-            robotOmega = np.arctan2(robotY_precedent - robotY, robotX_precedent - robotX)
-            robotTheta = robotOmega_precedent + robotOmega
-            robotTheta = (robotTheta * 180 / math.pi) % 360
-            
-            for i in range (0, 1000) :
-            	a = 100 + 100
+        robotX_precedent = robotX
+        robotY_precedent = robotY
+        robotX = int(M["m10"] / M["m00"])    
+        robotY = int(M["m01"] / M["m00"])
+        robotOmega_precedent = robotOmega_precedent + robotOmega
+        robotOmega = np.arctan2(robotY_precedent - robotY, robotX_precedent - robotX)
+        robotTheta = robotOmega_precedent + robotOmega
+        robotTheta = (robotTheta * 180 / math.pi) % 360
+
+    for i in range (0, 1000) :
+    a = 100 + 100
 
 
     print("robotX = " + str(robotX))
@@ -87,8 +88,8 @@ while OK is True :
     print("robotYprecedent = " + str(robotY_precedent))
     print("robot theta = " + str(robotTheta))
 '''
-	
-    
+
+
 
 
 
@@ -177,28 +178,28 @@ while OK is True :
 
 '''
 def find_no_from_coord(dimX, dimY) :
-    current_node = 0
-    last_node = dimX*dimY - 1
+current_node = 0
+last_node = dimX*dimY - 1
 
-    while (current_node != last_node) :
-        if carte.graph.listOfNodes[current_node].x == start_node_x :
-            #print("x ok")
+while (current_node != last_node) :
+if carte.graph.listOfNodes[current_node].x == start_node_x :
+#print("x ok")
 
-            #print(current_node)
-            if carte.graph.listOfNodes[current_node].y == start_node_y :
-                #print("y ok")
-                start_node = current_node
-                break
-            else : 
-                current_node = current_node + 1
-        else : 
-            #print(current_node)
-            current_node = current_node + 1
-    
-    #print("start node x = " +str(carte.graph.listOfNodes[start_node].x))
-    #print("start node y = " +str(carte.graph.listOfNodes[start_node].y))
+#print(current_node)
+if carte.graph.listOfNodes[current_node].y == start_node_y :
+#print("y ok")
+start_node = current_node
+break
+else : 
+current_node = current_node + 1
+else : 
+#print(current_node)
+current_node = current_node + 1
+
+#print("start node x = " +str(carte.graph.listOfNodes[start_node].x))
+#print("start node y = " +str(carte.graph.listOfNodes[start_node].y))
 
 
-    return current_node
+return current_node
 '''
 #################################################
