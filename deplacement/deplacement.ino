94476709b4b3aed57a16fd95d2f1b8b5528c8797
RichardTaquet <62691950+RichardTaquet@users.noreply.github.com>
2022-01-31 15:44:46 +0100

    Update deplacement.ino

--- deplacement/deplacement.ino
+++ deplacement/deplacement.ino
@@ -50,23 +50,6 @@ long uR = 0;
 // instantiate an object for the nRF24L01 transceiver
 RF24 radio(22, 23); // using pin 22 for the CE pin, and pin 23 for the CSN pin
 
-// Let these addresses be used for the pair
-uint8_t address[][6] = {"1Node", "2Node"};
-// It is very helpful to think of an address as a path instead of as
-// an identifying device destination
-
-// to use different addresses on a pair of radios, we need a variable to
-// uniquely identify which address this radio will use to transmit
-bool radioNumber = 1; // 0 uses address[0] to transmit, 1 uses address[1] to transmit
-
-// Used to control whether this node is sending or receiving
-bool role = false;  // true = TX role, false = RX role
-
-// For this example, we'll be using a payload containing
-// a single float number that will be incremented
-// on every successful transmission
-
-
 void setup() {
 
   Serial.begin(9600);
