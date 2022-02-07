d2ba267593df2237fea2a400bd4cc623f390a340
Raphael KOSKAS <rkoskas@et.esiea.fr>
2022-02-07 10:15:29 +0100

    comments in get_coord()

--- main/functions.py
+++ main/functions.py
@@ -1,34 +1,23 @@
 import cv2 as cv
 import numpy as np
 
-# every loop functions
 def get_coord(cap, capX, capY, robot, current_frame) :
-
+    # recuperation d'une frame
     success, frame = cap.read()
     if not success :
         print("cap read not successed")
         exit()
-
+    
+    # traitement de l'image
     frame = cv.resize(frame, (capX,capY))
-    # step 3 : cvtColor
     frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    # step 4 : blur
     frame = cv.blur(frame, (5,3))
-    # step 5 : turn into binary frame
     success, frame = cv.threshold(frame,127,255,0)
     if not success :
         print("threshold not successed")
         exit()
 
-    cv.imwrite('frames/'+str(current_frame)+ '.jpg', frame)  # save frame as JPEG file
-    current_frame += 1
-
-    #"""
-    cv.imshow('frame', frame)
-    if cv.waitKey(1) == ord('q') :
-        exit()
-    #"""
-    # step 6 : get lighting point coordinates
+    # récupèration des coordonnées du point lumineux
     contours, hierarchy = cv.findContours(frame, cv.RETR_TREE,cv.CHAIN_APPROX_TC89_KCOS)
     for c in contours:
         M = cv.moments(c)
@@ -38,11 +27,18 @@ def get_coord(cap, capX, capY, robot, current_frame) :
             robotY = int(M["m01"] / M["m00"])
             robot.y = int(robotY)
 
+    # stockage de l'image pour post production
+    cv.imwrite('frames/'+str(current_frame)+ '.jpg', frame)  # save frame as JPEG file
+    current_frame += 1
+
+    # affichage dans une fenêtre
+    cv.imshow('frame', frame)
+    if cv.waitKey(1) == ord('q') :
+        exit()
+
 
 def map(x, in_min, in_max, out_min, out_max):
     return int((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min)
-
-
 
 
 def debug(robot, WPManager):
